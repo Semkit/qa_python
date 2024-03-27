6e06e7f0b0619c1780b908d723a5542f78d1ddca
Semkit <semkit23@gmail.com>
2024-03-27 21:26:59 +0300

    9 unit tests added

--- tests.py
+++ tests.py
@@ -1,24 +1,88 @@
+import pytest
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
     def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
+
         collector = BooksCollector()
 
-        # добавляем две книги
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        assert len(collector.get_books_genre()) == 2
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_get_book_genre_positive(self):
+
+        collector = BooksCollector()
+        collector.add_new_book('Задача трёх тел')
+        collector.set_book_genre('Задача трёх тел', 'Фантастика')
+        assert collector.get_book_genre('Задача трёх тел') == 'Фантастика'
+
+    def test_set_book_genre_change_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Задача трёх тел')
+        collector.set_book_genre('Задача трёх тел', 'Ужасы')
+        collector.set_book_genre('Задача трёх тел', 'Фантастика')
+
+        assert collector.books_genre.get('Задача трёх тел') == 'Фантастика'
+
+    def test_add_new_book_no_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Задача трёх тел')
+
+        assert collector.books_genre.get('Задача трёх тел') == ''
+
+    def test_get_books_with_specific_genre_positive(self):
+        collector = BooksCollector()
+        collector.add_new_book('Задача трёх тел')
+        collector.set_book_genre('Задача трёх тел', 'Фантастика')
+        collector.add_new_book('Красная шапочка')
+        collector.set_book_genre('Красная шапочка', 'Мультфильмы')
+        collector.add_new_book('Ночной дозор')
+        collector.set_book_genre('Ночной дозор', 'Фантастика')
+        assert len(collector.get_books_with_specific_genre('Фантастика')) == 2
+
+    def test_get_books_for_children_with_every_genre(self):
+        collector = BooksCollector()
+        collector.books_genre = {
+            'Мастер и Маргарита': 'Фантастика',
+            'Сияние': 'Ужасы',
+            'Пуаро': 'Детективы',
+            'Дживс и Вустер': 'Комедии',
+            'Капитан Врунгель': 'Мультфильмы'
+        }
+        assert 'Сияние' and 'Пуаро' not in collector.get_books_for_children()
+
+    def test_add_book_in_favorites_one_positive_one_negative(self):
+        collector = BooksCollector()
+        collector.books_genre = {
+            'Мастер и Маргарита': 'Фантастика',
+            'Сияние': 'Ужасы',
+            'Пуаро': 'Детективы',
+            'Дживс и Вустер': 'Комедии',
+            'Капитан Врунгель': 'Мультфильмы'
+        }
+        collector.add_book_in_favorites('Мастер и Маргарита')
+        collector.add_book_in_favorites('Ночной дозор')
+        assert len(collector.get_list_of_favorites_books()) == 1
+
+    def test_delete_book_from_favorites_one_book(self):
+        collector = BooksCollector()
+        collector.favorites.append('Мастер и Маргарита')
+        collector.delete_book_from_favorites('Мастер и Маргарита')
+        assert len(collector.get_list_of_favorites_books()) == 0
+
+    @pytest.mark.parametrize('genre', ['Мьюзиклы', 'Обучающие','Исторические'])
+    def test_get_book_with_specific_genre_negative(self, genre):
+        collector = BooksCollector()
+        collector.books_genre = {
+            'Мастер и Маргарита': 'Фантастика',
+            'Сияние': 'Ужасы',
+            'Пуаро': 'Детективы',
+            'Дживс и Вустер': 'Комедии',
+            'Капитан Врунгель': 'Мультфильмы'
+        }
+        assert collector.get_books_with_specific_genre(genre) == []
+
